441bbf08ffdb9ce421fc19c0d57123d38d1b8929
Rodic <ixr2102@g.rit.edu>
2020-05-07 01:05:18 +0200

    add changes to bot.js

--- src/bot.js
+++ src/bot.js
@@ -26,9 +26,9 @@ client.on('message', function (message) {
         let embed = {
             "title": "Welcome to the CSGORolls Support Bot",
             "description": "Hello there, \nI am here to help you in case you're having some issues with using CSGORoll. Down below I will list the most common issues that may occur while using our services. If you're experiencing an issue that is listed below, please use the command stated above the issue for the possible solution.",
-            "url": "https://discordapp.com",
+            "url": "https://www.csgoroll.com/en/support",
             "color": 2172203,
-            "timestamp": "2020-05-06T15:55:00.710Z",
+            "timestamp": new Date(),
 
             "thumbnail": {
                 "url": client.user.avatarURL()
@@ -80,7 +80,7 @@ client.on('message', function (message) {
             "title": "You need to be at least level 5 on steam in order to be able to claim FREE BOX error",
             "description": "Hey there, \n\nPlease change your profile information to public:\n\nNavigate yourself to your steam profile;\nPress the 'Edit profile' menu;\nSelect 'My Privacy Settings';\nSet all information to public.\n\nOn CSGORoll:\n\nGo to your Profile summary;\nUpdate the Steam Trade URL;\nEnjoy!\n\n *IMPORTANT* \nYour profile can take 60 minutes to be updated and make sure you have at least 1 valid item in your CS:GO inventory that can be sold on steam market.",
             "color": 2172203,
-            "timestamp": "2020-05-06T18:11:46.678Z",
+            "timestamp":new Date(),
             "thumbnail": {
                 "url": client.user.avatarURL()
             }
@@ -93,7 +93,7 @@ client.on('message', function (message) {
             "title": "The website can not load my inventory. What to do?",
             "description": "Hey there, \nThis issue usually happens when steam is having connection issues, making the inventory take a bit longer than usual to load. \n\nKeep refreshing your page and it should work at some point or try again later when inventories are no longer critical https://steamstat.us/.",
             "color": 2172203,
-            "timestamp": "2020-05-06T18:11:46.678Z",
+            "timestamp": new Date(),
             "thumbnail": {
                 "url": client.user.avatarURL()
             }
@@ -107,7 +107,7 @@ client.on('message', function (message) {
             "title": "I have deposited but I still didn't get credited",
             "description": "Hey there​​, \nPlease create a support ticket at https://www.csgoroll.com/en/support or email us directly at support@csgoroll.com with the following information:\n\nScreenshots of the transaction (Can access from your Steam inventory -> Trade history)\n\nSteam profile of the user you traded with. - Right click the page and select 'Copy Page URL'.",
             "color": 2172203,
-            "timestamp": "2020-05-06T18:11:46.678Z",
+            "timestamp": new Date(),
             "thumbnail": {
                 "url": client.user.avatarURL()
             }
@@ -121,7 +121,7 @@ client.on('message', function (message) {
             "title": "I have deposited but I still didn't get credited",
             "description": "Hey there, \n\nItems that don't have a withdraw button aren't being stocked by us.\nYou can only withdraw them if someone were to deposit that exact skin to one of our traders.\n\n\nItem that -can- be withdrawn will always have a timer or 'Instant' tag on it.\nWhen these timers run out you can click 'Collect Item' and send a trade for the correct item which will then be automatically accepted.",
             "color": 2172203,
-            "timestamp": "2020-05-06T18:11:46.678Z",
+            "timestamp": new Date(),
             "thumbnail": {
                 "url": client.user.avatarURL()
             }
@@ -135,7 +135,7 @@ client.on('message', function (message) {
             "title": "I'm interested in partnership",
             "description": "Hey there, \n\n If you want to create content for us, please DM us your offer on our twitter (https://twitter.com/CSGORoll)",
             "color": 2172203,
-            "timestamp": "2020-05-06T18:11:46.678Z",
+            "timestamp": new Date(),
             "thumbnail": {
                 "url": client.user.avatarURL()
             }
@@ -149,7 +149,7 @@ client.on('message', function (message) {
             "title": "Frequently Asked Questions",
             "description": "Hey there, \n\n If you wish to check out our FAQ section, please visit this link -> https://www.csgoroll.com/en/info/faq",
             "color": 2172203,
-            "timestamp": "2020-05-06T18:11:46.678Z",
+            "timestamp": new Date(),
             "thumbnail": {
                 "url": client.user.avatarURL()
             }
@@ -163,7 +163,7 @@ client.on('message', function (message) {
             "title": "Frequently Asked Questions",
             "description": "Hey there, \n\n CSGORoll currently applies a daily limit of 200 coins over your deposit value to be able to withdraw. This is just a temporary precaution whilst in Beta.\nYou can simply request an increase by withdrawing one of your items that are over this limit. A pop-up will appear where you must type the extra value you'd like to request and our staff will manually approve it!\n *IMPORTANT* \n Make sure to type a rounded number so it doesn't glitch, this request can take a few hours to be reviewed and we are not able to speed up the process here.",
             "color": 2172203,
-            "timestamp": "2020-05-06T18:11:46.678Z",
+            "timestamp": new Date(),
             "thumbnail": {
                 "url": client.user.avatarURL()
             }
@@ -177,7 +177,7 @@ client.on('message', function (message) {
             "title": "Frequently Asked Questions",
             "description": "Hey there, \n\n In order to be eligible to use our withdraw services, you need to deposit at least 1 coin. \n *IMPORTANT* \n When it comes to CS:GO and Dota2 skins, we only accept skins that are worth at least 2$ each.",
             "color": 2172203,
-            "timestamp": "2020-05-06T18:11:46.678Z",
+            "timestamp": new Date(),
             "thumbnail": {
                 "url": client.user.avatarURL()
             }
